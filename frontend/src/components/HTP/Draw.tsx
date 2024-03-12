cf6418efe17fb45f5b971ab834ecf16632d419a4
yeji <yeji1107@naver.com>
2024-03-12 16:13:52 +0900

    S10P22A303-184 [feat] 파일업로드 기능, 메인화면에 게시판 3d 모델 띄우기

---
+++ frontend/src/components/HTP/Draw.tsx
@@ -0,0 +1,10 @@
+import CanvasDraw from "react-canvas-draw"
+
+// https://embiem.github.io/react-canvas-draw/
+function Draw() {
+  return(
+    <CanvasDraw canvasWidth={500} canvasHeight={500}/>
+  )
+}
+
+export default Draw
--- frontend/src/pages/HTP/House.tsx
+++ frontend/src/pages/HTP/House.tsx
@@ -1,11 +1,14 @@
-import { useState, createRef } from "react"
+import { useState, useRef, useEffect } from "react"
 import { Button } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import Draw from "../../components/HTP/Draw";
 
 
 // props의 타입을 지정해주자
 type propsType = {
-  goNext: React.MouseEventHandler<HTMLButtonElement>
+  // goNext: React.MouseEventHandler<HTMLButtonElement>
+  goNext: () => void
 }
 
 // 집 그림 그리고, 설문조사가 나오는 페이지
@@ -29,12 +32,32 @@ function House() {
 export default House
 
 function House0({ goNext }: propsType) {
+  const [file, setFile] = useState<File|null>(null)
+  const fileInput = useRef<HTMLInputElement>(null);
+  const handleChange = (event :React.ChangeEvent<HTMLInputElement>) => {
+    // 선택한 파일 정보를 저장
+    const files = event.target.files
+    if (files && files[0]) {
+      setFile(files[0])
+    }
+  };
+
+  // 써먹을려면
+  useEffect(()=> {
+    if (file != null) {
+      console.log(file)
+      Swal.fire({
+        title: '업로드완료',
+        icon: "success"
+      }).then((r) => {goNext()})
+    }
+  }, [file])
 
   return (
     <div className="flex h-svh w-svh justify-center items-center flex-col">
       <p className="text-center mb-8 font-bold text-3xl">집을 그려주세요.</p>
       <div className="relative border-2 rounded h-2/3 w-5/6">
-        <p>집그리는 div</p>
+        <Draw />
         <Button className="absolute bottom-0 right-0 m-3" onClick={goNext}>다 그렸어요</Button>
       </div>
       <div className="flex items-center text-slate-500 mt-2">
@@ -43,10 +66,12 @@ function House0({ goNext }: propsType) {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
         </svg>
-        <div>
+        <button onClick={() => {fileInput.current?.click()}}>
           <p className="underline ml-1 hover:text-violet-500 hover:cursor-pointer">업로드하기</p>
-        </div>
+        </button>
       </div>
+      {/* png형식의 파일만 올릴 수 있도록 함 */}
+      <input type="file" accept=".png" ref={fileInput} onChange={handleChange} style={{display:'none'}}/>
     </div>
   )
 }
