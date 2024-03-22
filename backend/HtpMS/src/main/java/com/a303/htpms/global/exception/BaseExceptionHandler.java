5d41e0442eebf913bf6e0c6f629187480e7e541c
하동준 <djh0211@konkuk.ac.kr>
2024-03-22 11:20:21 +0900

    [feat] htpms init * [feat] htpms init

---
+++ backend/HtpMS/src/main/java/com/a303/htpms/global/exception/BaseExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.a303.htpms.global.exception;
+
+import com.a303.htpms.global.exception.code.ErrorCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class BaseExceptionHandler extends RuntimeException {
+    private final ErrorCode errorCode;
+    @Setter
+    private String message;
+
+    public BaseExceptionHandler(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public BaseExceptionHandler(String message, ErrorCode errorCode) {
+        super(message);
+        this.message = message;
+        this.errorCode = errorCode;
+    }
+}
