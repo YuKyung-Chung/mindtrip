a62d05e1f013945567d8af633603ea8572927a1a
chico <silverstuff@naver.com>
2024-03-22 16:03:02 +0900

    [fix] missiontree 타입오류 수정

--- frontend/src/components/Loading/MissionTree.tsx
+++ frontend/src/components/Loading/MissionTree.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './MissionTree.css'; // Tree 컴포넌트에 대한 CSS 파일을 임포트합니다.
 
+// CSS 변수를 포함한 스타일의 타입을 정의합니다.
+type TreeStyle = React.CSSProperties & {
+  '--x': number;
+  '--i': number;
+};
+
 const Tree: React.FC = () => {
   return (
     <div className="container">
       <div className="tree">
         {[0, 1, 2, 3].map((x) => (
-          <div key={x} className="branch" style={{ '--x': x }}>
+          <div key={x} className="branch" style={{ '--x': x } as TreeStyle}>
             {[0, 1, 2, 3].map((i) => (
-              <span key={i} style={{ '--i': i }}></span>
+              <span key={i} style={{ '--i': i } as TreeStyle}></span>
             ))}
           </div>
         ))}
         <div className="stem">
           {[0, 1, 2, 3].map((i) => (
-            <span key={i} style={{ '--i': i }}></span>
+            <span key={i} style={{ '--i': i } as TreeStyle}></span>
           ))}
         </div>
         <span className="shadow"></span>
