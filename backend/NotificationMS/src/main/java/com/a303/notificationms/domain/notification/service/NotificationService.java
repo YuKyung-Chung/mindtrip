0861e000567cd6cb68b7afaa3afe598c32a8aa2d
하동준 <djh0211@konkuk.ac.kr>
2024-04-07 22:52:10 +0900

    Feat/migrate noti firebase * [hot-fix] notifyms - fcm overtimed token delete
    * [feat] notifyms - fcm 적용
    
    * S10P22A303-295 [feat] 써킷브레이커

--- backend/NotificationMS/src/main/java/com/a303/notificationms/domain/notification/service/NotificationService.java
+++ backend/NotificationMS/src/main/java/com/a303/notificationms/domain/notification/service/NotificationService.java
@@ -1,15 +1,10 @@
 package com.a303.notificationms.domain.notification.service;
 
-import com.a303.notificationms.domain.notification.dto.request.FCMNotificationReq;
 import com.a303.notificationms.domain.notification.dto.response.NotificationMessageRes;
 import java.util.List;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 public interface NotificationService {
-
-	SseEmitter subscribe(int memberId);
-
-	void notifyCnt(int memberId);
 
 	List<NotificationMessageRes> findNotificationsByMemberId(int memberId);
 
--- backend/NotificationMS/src/main/java/com/a303/notificationms/domain/notification/service/NotificationServiceImpl.java
+++ backend/NotificationMS/src/main/java/com/a303/notificationms/domain/notification/service/NotificationServiceImpl.java
@@ -1,6 +1,5 @@
 package com.a303.notificationms.domain.notification.service;
 
-import com.a303.notificationms.domain.Emitter.repository.EmitterRepository;
 import com.a303.notificationms.domain.domain.Domain;
 import com.a303.notificationms.domain.domain.repository.DomainRepository;
 import com.a303.notificationms.domain.memberToken.MemberToken;
@@ -52,7 +51,6 @@ public class NotificationServiceImpl implements NotificationService {
 //	@Autowired
 //	private final EntityManager em;
 
-	private final EmitterRepository emitterRepository;
 	private final NotificationRepository notificationRepository;
 	private final DomainRepository domainRepository;
 	private final MemberClient memberClient;
@@ -62,59 +60,6 @@ public class NotificationServiceImpl implements NotificationService {
 
 	private final FirebaseMessaging firebaseMessaging;
 
-	// 메시지 알림
-	public SseEmitter subscribe(int memberId) {
-
-		// 1. 현재 클라이언트를 위한 sseEmitter 객체 생성
-		SseEmitter sseEmitter = new SseEmitter((long) (2 * 1000));
-
-		// 2. 연결
-		try {
-			sseEmitter.send(SseEmitter.event().name("connect"));
-//			log.info("connect");
-		} catch (IOException e) {
-//			e.printStackTrace();
-		}
-
-		// 3. 저장
-		emitterRepository.save(memberId, sseEmitter);
-
-		// 4. 연결 종료 처리
-		sseEmitter.onCompletion(
-			() -> emitterRepository.removeByMemberId(memberId));    // sseEmitter 연결이 완료될 경우
-		sseEmitter.onTimeout(() -> emitterRepository.removeByMemberId(
-			memberId));        // sseEmitter 연결에 타임아웃이 발생할 경우
-		sseEmitter.onError((e) -> emitterRepository.removeByMemberId(
-			memberId));        // sseEmitter 연결에 오류가 발생할 경우
-
-		return sseEmitter;
-	}
-
-	public void notifyCnt(int memberId) {
-
-		// memberId로 저장된 미개봉 notification 개수 조회
-		Long notificationCnt = notificationRepository.countByMemberIdAndAndIsWritten(memberId,
-			false);
-
-		// 7. Map 에서 userId 로 사용자 검색
-		SseEmitter sseEmitterReceiver = emitterRepository.findByMemberId(memberId);
-		if (sseEmitterReceiver == null) {
-			throw new BaseExceptionHandler("SseEmitter 발견 불가", ErrorCode.NOT_FOUND_ERROR);
-		}
-
-		// 8. 알림 메시지 전송 및 해체
-		try {
-			CountNotificationMessageRes messageRes = CountNotificationMessageRes.builder()
-				.type("COUNT")
-				.count(notificationCnt)
-				.localDateTime(LocalDateTime.now())
-				.build();
-			sseEmitterReceiver.send(SseEmitter.event().name("message").data(messageRes));
-
-		} catch (Exception e) {
-			emitterRepository.removeByMemberId(memberId);
-		}
-	}
 
 	@Override
 	public List<NotificationMessageRes> findNotificationsByMemberId(int memberId) {
@@ -260,6 +205,8 @@ public class NotificationServiceImpl implements NotificationService {
 					firebaseMessaging.send(message1);
 					firebaseMessaging.send(message2);
 				} catch (FirebaseMessagingException e) {
+
+					memberToken.getTokens().remove(token);
 //					throw new BaseExceptionHandler(e.toString(), ErrorCode.FCM_IO_EXCEPTION);
 				}
 			}
@@ -308,7 +255,8 @@ public class NotificationServiceImpl implements NotificationService {
 				firebaseMessaging.send(message1);
 				firebaseMessaging.send(message2);
 			} catch (FirebaseMessagingException e) {
-				throw new BaseExceptionHandler(e.toString(), ErrorCode.FCM_IO_EXCEPTION);
+				memberToken.getTokens().remove(token);
+//				throw new BaseExceptionHandler(e.toString(), ErrorCode.FCM_IO_EXCEPTION);
 			}
 		}
 
@@ -361,7 +309,8 @@ public class NotificationServiceImpl implements NotificationService {
 				firebaseMessaging.send(message1);
 				firebaseMessaging.send(message2);
 			} catch (FirebaseMessagingException e) {
-				throw new BaseExceptionHandler(e.toString(), ErrorCode.FCM_IO_EXCEPTION);
+				memberToken.getTokens().remove(token);
+//				throw new BaseExceptionHandler(e.toString(), ErrorCode.FCM_IO_EXCEPTION);
 			}
 		}
 
@@ -391,7 +340,8 @@ public class NotificationServiceImpl implements NotificationService {
 			try {
 				firebaseMessaging.send(message);
 			} catch (FirebaseMessagingException e) {
-				throw new BaseExceptionHandler(e.toString(), ErrorCode.FCM_IO_EXCEPTION);
+				memberToken.getTokens().remove(token);
+//				throw new BaseExceptionHandler(e.toString(), ErrorCode.FCM_IO_EXCEPTION);
 			}
 		}
 	}
